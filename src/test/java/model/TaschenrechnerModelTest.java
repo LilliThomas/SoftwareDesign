480dbec27b2dfc9bd05e8c136f5f1276b2831ab5
RikiiB <143796850+RikiiB@users.noreply.github.com>
2024-05-13 18:02:02 +0200

    removed unused imports

--- src/test/java/model/TaschenrechnerModelTest.java
+++ src/test/java/model/TaschenrechnerModelTest.java
@@ -1,6 +1,5 @@
 package model;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;

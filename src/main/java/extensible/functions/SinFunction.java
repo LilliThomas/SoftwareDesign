5c27a0a5381f45e8f28dc27c0e09db608d8d49ba
RikiiB <henrike.bickel@gmail.com>
2024-05-24 19:28:55 +0200

    added constants

--- src/main/java/extensible/functions/SinFunction.java
+++ src/main/java/extensible/functions/SinFunction.java
@@ -1,6 +1,7 @@
 package extensible.functions;
 
 public class SinFunction implements CalculatorFunction {
+    public static final String SIN_FUNCTION = "SinFunction";
     @Override
     public double calculate(double parameter) {
         return Math.sin(parameter);

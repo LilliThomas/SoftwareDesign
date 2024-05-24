3a48c8ae9e0016902f66b30b72dc2b1d786a88f9
RikiiB <henrike.bickel@gmail.com>
2024-05-24 19:28:55 +0200

    added constants

--- src/main/java/extensible/functions/CosFunction.java
+++ src/main/java/extensible/functions/CosFunction.java
@@ -1,6 +1,7 @@
 package extensible.functions;
 
 public class CosFunction implements CalculatorFunction {
+    public static final String COS_FUNCTION = "CosFunction";
     @Override
     public double calculate(double parameter) {
         return Math.cos(parameter);

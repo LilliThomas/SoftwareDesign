e47a8cb77d73fa4e6082e5303eb07853e967f577
LilliThomas <lilli.thomas@hsw-stud.de>
2024-06-07 18:34:54 +0200

    javadoc DivisionException

--- src/main/java/exceptions/DivisionException.java
+++ src/main/java/exceptions/DivisionException.java
@@ -1,7 +1,10 @@
 package exceptions;
 
 public class DivisionException extends RuntimeException {
-
+    /**
+     * occurs when divided by 0
+     * @param message a message is transferred during the call
+     */
     public DivisionException(String message) {
         super(message);
     }

0cc98c61a7b0431369e9c533e771c38e868c87d6
LilliThomas <lilli.thomas@hsw-stud.de>
2024-06-07 18:40:12 +0200

    javadoc ReflectionException

--- src/main/java/exceptions/ReflectionException.java
+++ src/main/java/exceptions/ReflectionException.java
@@ -1,7 +1,10 @@
 package exceptions;
 
 public class ReflectionException extends RuntimeException {
-
+    /**
+     * Occurs when an expandable function is selected that cannot be loaded.
+     * @param message a message is transferred during the call
+     */
     public ReflectionException(String message) {
         super(message);
     }
